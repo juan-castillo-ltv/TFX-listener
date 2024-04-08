3e10239d6e375e0a872dffb835b78a03c0e4c185
juan-castillo-ltv <juan.castillo@ltv.fund>
2024-04-08 15:30:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ def track_event():
     return jsonify({"success": "Event tracked successfully"}), 200
 
 if __name__ == '__main__':
-    app.run()  # (debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
