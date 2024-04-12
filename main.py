bcdd5dc12c56b60a742bf5667971df40b868b1f3
juan-castillo-ltv <juan.castillo@ltv.fund>
2024-04-12 11:53:03 -0500

    Update main.py
    Updated main script to use a connection pool.

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 from flask import Flask, request, jsonify
 import psycopg2
+import psycopg2.pool
 import logging
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 from config import DB_CREDENTIALS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS on the Flask app
+logging.basicConfig(level=logging.INFO)
 
-def get_db_connection():
-    return psycopg2.connect(
-        host=DB_CREDENTIALS['host'],
-        port=DB_CREDENTIALS['port'],
-        dbname=DB_CREDENTIALS['database'],
-        user=DB_CREDENTIALS['user'],
-        password=DB_CREDENTIALS['password'],
-        sslmode=DB_CREDENTIALS['sslmode']  
-    )
+# Set up a connection pool
+connection_pool = psycopg2.pool.SimpleConnectionPool(1, 10,  # minconn, maxconn
+                                                     host=DB_CREDENTIALS['host'],
+                                                     port=DB_CREDENTIALS['port'],
+                                                     dbname=DB_CREDENTIALS['database'],
+                                                     user=DB_CREDENTIALS['user'],
+                                                     password=DB_CREDENTIALS['password'],
+                                                     sslmode=DB_CREDENTIALS['sslmode'])
+
 @app.route('/track', methods=['POST'])
-
 def track_event():
-    event_data = request.json
-    conn = get_db_connection()
+    event_data = request.get_json()
+    if not event_data:
+        return jsonify({"error": "Invalid data"}), 400
+
+    conn = connection_pool.getconn()
+    if conn is None:
+        logging.error("Failed to connect to the database")
+        return jsonify({"error": "Database connection error"}), 500
+    
     cur = conn.cursor()
     try:
         cur.execute("""
-            INSERT INTO app_click_events (event_type, event_name, user_id, utm_source, utm_medium, utm_campaign, utm_content, time_of_event,event_url,app)
+            INSERT INTO app_click_events (event_type, event_name, user_id, utm_source, utm_medium, utm_campaign, utm_content, time_of_event, event_url, app)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (
             event_data.get('eventType'),
@@ -34,22 +42,21 @@ def track_event():
             event_data.get('utmMedium'),
             event_data.get('utmCampaign'),
             event_data.get('utmContent'),
-            event_data.get('timeOfEvent'), # Ensure this is formatted correctly for PostgreSQL
+            event_data.get('timeOfEvent'),
             event_data.get('eventUrl'),
-            event_data.get('appName') 
+            event_data.get('appName')
         ))
-        logging.info("Event tracked successfully")
         conn.commit()
+        logging.info("Event tracked successfully")
     except Exception as e:
-        print(e)
-        conn.rollback()
         logging.error(f"Failed to insert event data: {e}")
+        conn.rollback()
         return jsonify({"error": "Failed to insert event data"}), 500
     finally:
         cur.close()
-        conn.close()
-        logging.info("Event tracked successfully. Connection closed")
+        connection_pool.putconn(conn)
+    
     return jsonify({"success": "Event tracked successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
